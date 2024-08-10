f5e35e8487adb567ffadf7ed61d92b798c19190b
Aqsa Mumtaz <codewithaqsa@gmail.com>
2024-08-10 16:09:48 +0500

    path-parameters

--- express/index.js
+++ express/index.js
@@ -10,5 +10,47 @@ app.listen(port, () =>{
 
 
 app.use((req, res)=>{
-    console.log("request recieved")
+
+    console.log("request recieved");
+    res.send({
+        fruit: "apple",
+        color: "red",
+        price : 250
+    })
 }) 
+
+app.get("/" , (req, res) =>{
+    res.send("you connected to the root path")
+})
+
+// this code is for path parameters
+
+app.get("/:username/:id", (req, res) => {
+    console.log(req.params);
+    res.send("You connected again ")
+})
+
+app.get("/search", (req, res) =>{
+    console.log(req.query);
+    res.send("no results")
+})
+
+app.get("/about", (req, res) =>{
+    res.send("this is the about page")
+})
+
+app.get("/services", (req, res) =>{
+    res.send("this is the services page")
+})
+
+app.get("/contact", (req, res) =>{
+    res.send("this is the contact page")
+})
+
+app.get ("*" , (req, res) =>{
+    res.send("404 page not found")
+})
+
+app.post("/" , (req,res) =>{
+    res.send("you connected to the root path with post method")
+})
