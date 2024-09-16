5c42b56f82370e2cfc68ce76c3980bc21103a533
Aqsa Mumtaz <codewithaqsa@gmail.com>
2024-09-16 18:52:17 +0500

    sql

---
+++ Sql/index.js
@@ -0,0 +1,29 @@
+const { faker } = require("@faker-js/faker");
+const mysql = require("mysql2");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  database: "DELTA_app",
+  password: "Mernstack@2025",
+});
+
+try {
+  connection.query("SHOW TABLES", (err, result) => {
+    if (err) throw err;
+    console.log(result);
+  });
+} catch (error) {
+  console.log(error);
+}
+
+let getRandomUser = () => {
+  return {
+    userId: faker.string.uuid(),
+    username: faker.internet.userName(),
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+  };
+};
+
+
